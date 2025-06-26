cea9c74db60e7fd831e795937d840424e7ad2e2e
Sahas2711 <sahasnagar1234@gmail.com>
2025-06-26 22:31:45 +0530

    Sahas : First commit

---
+++ src/main/java/com/mit/StayNest/Entity/Review.java
@@ -0,0 +1,87 @@
+package com.mit.StayNest.Entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id 
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "tenant_id")
+    private User tenant;
+    @ManyToOne
+    @JoinColumn(name = "listing_id")
+    private Listing listing;
+    private int rating; 
+    private String feedback;
+    private LocalDateTime createdAt;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public User getTenant() {
+		return tenant;
+	}
+	public void setTenant(User tenant) {
+		this.tenant = tenant;
+	}
+	public Listing getListing() {
+		return listing;
+	}
+	public void setListing(Listing listing) {
+		this.listing = listing;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getFeedback() {
+		return feedback;
+	}
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Review() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Review(Long id, User tenant, Listing listing, int rating, String feedback, LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.tenant = tenant;
+		this.listing = listing;
+		this.rating = rating;
+		this.feedback = feedback;
+		this.createdAt = createdAt;
+	}
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", rating=" + rating + ", feedback=" + feedback + ", createdAt=" + createdAt + "]";
+	}
+    
+    @PrePersist
+    public void onCreate() {
+    	createdAt = LocalDateTime.now();
+    }
+}
+
---
+++ src/main/java/com/mit/StayNest/Services/ReviewService.java
@@ -0,0 +1,8 @@
+package com.mit.StayNest.Services;
+
+import java.util.List;
+
+import com.mit.StayNest.Entity.Review;
+
+public interface ReviewService {
+}
