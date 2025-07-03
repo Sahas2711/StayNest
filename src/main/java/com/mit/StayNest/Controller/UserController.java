3382165568c56a99e5a6499b302fa4a329fe904d
DELL <DELL@DESKTOP-MT52VAD>
2025-07-03 16:50:16 +0530

    Listings

--- src/main/java/com/mit/StayNest/Controller/UserController.java
+++ src/main/java/com/mit/StayNest/Controller/UserController.java
@@ -1,37 +1,7 @@
-<<<<<<< HEAD
-package com.mit.StayNest.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.resource.HttpResource;
 
-import com.mit.StayNest.Entity.User;
-import com.mit.StayNest.Services.UserService;
 
-@RestController
-public class UserController {
 
-	@Autowired
-	UserService userService ;
-	
-    @PostMapping("/register")
-    public User register(@RequestBody User request) {
-        // registration logic here
-        return this.userService.register(request);
-    }
-
-    @PostMapping("/login")
-    public User login(@RequestBody User request) {
-        // login logic here
-        return this.userService.login(request);
-    }
-}
-
-=======
 package com.mit.StayNest.Controller;
 
 import java.util.List;
@@ -72,4 +42,3 @@ public class UserController {
     }
 }
 
->>>>>>> branch 'main' of https://github.com/Sahas2711/StayNest.git
--- src/main/java/com/mit/StayNest/Services/UserServiceImpl.java
+++ src/main/java/com/mit/StayNest/Services/UserServiceImpl.java
@@ -1,50 +1,4 @@
-<<<<<<< HEAD
-package com.mit.StayNest.Services;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.mit.StayNest.Entity.User;
-import com.mit.StayNest.Repository.UserRepository;
-
-@Service
-public class UserServiceImpl implements UserService {
-
-    @Autowired
-    private UserRepository userRepo;
-
-    @Override
-    public List<User> getUser() {
-        return userRepo.findAll();
-    }
-
-    @Override
-    public User register(User user) {
-        // Optional: Check if user already exists (e.g. by email)
-        Optional<User> existingUser = userRepo.findByEmail(user.getEmail());
-        if (existingUser.isPresent()) {
-            throw new RuntimeException("User already exists with email: " + user.getEmail());
-        }
-
-        // Save user to DB
-        return userRepo.save(user);
-    }
-
-    @Override
-    public User login(User user) {
-        Optional<User> existingUser = userRepo.findByEmail(user.getEmail());
-
-        if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
-            return existingUser.get(); // Login success
-        }
-
-        throw new RuntimeException("Invalid email or password");
-    }
-}
-=======
 package com.mit.StayNest.Services;
 
 import java.util.List;
@@ -92,4 +46,4 @@ public class UserServiceImpl implements UserService {
         throw new RuntimeException("Invalid email or password");
     }
 }
->>>>>>> branch 'main' of https://github.com/Sahas2711/StayNest.git
+
